694bde8358026faf891fc0f8768a0cd45a9b3dc9
mari <marizxv@gmail.com>
2025-10-22 01:52:16 +0200

    Forgotten Main push

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -1,16 +1,16 @@
 package edu.io;
 
-import edu.io.token.Token;
+import edu.io.token.GoldToken;
+import edu.io.token.PlayerToken;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(5);
 
-        Token gold = new Token("\uD83D\uDCB0");
-        Token player = new Token("\uC6C3");
+        GoldToken gold = new GoldToken();
+        PlayerToken player = new PlayerToken(board, 3, 4);
 
         board.placeToken(1, 2, gold);
-        board.placeToken(3, 4, player);
 
         board.display();
     }
